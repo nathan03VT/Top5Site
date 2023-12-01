bf2f95b5e7e9a6f58b4b58d8a967c62ff3f9a924
Ijahmin <62042590+Ijahmin@users.noreply.github.com>
2023-12-01 14:03:22 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const top5Controller = require('./top5Controller');
 
 const app = express();
 const port = 3000;
@@ -7,21 +8,9 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-let userTop5s = [];
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.post('/addTop5', (req, res) => {
-    const newTop5 = req.body.top5;
-    userTop5s.push(newTop5);
-    res.redirect('/');
-});
-
-app.get('/getTop5s', (req, res) => {
-    res.json(userTop5s);
-});
+app.get('/', top5Controller.getIndexPage);
+app.post('/addTop5', top5Controller.addTop5);
+app.get('/getTop5s', top5Controller.getTop5s);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
